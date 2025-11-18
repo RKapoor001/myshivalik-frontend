74a65ce522b62bbeef9f29c1c5094481ce9c6b08
Rudra Kapoor <kapoorprojects2010@gmail.com>
2025-11-18 21:23:59 +0530

    Updated frontend version

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from "react-router-dom";
 import Search from "./Search";
 import "./App.css"; // ✅ import the color theme CSS
 import Signup from "./Signup";
